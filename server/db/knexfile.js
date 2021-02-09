9d03c2b5a5b9c2cfe75a3e661adc3396d1bf9e5f
skendanavian <s.nissen.bass@gmail.com>
2021-02-08 22:18:18 -0600

    db working

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASS,
-      database: process.env.DB_NAME,
+      host: "5432",
+      user: "labber",
+      password: "labber",
+      database: "dnaducation",
     },
     migrations: {
       directory: "./migrations",
