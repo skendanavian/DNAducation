dc4001a830492c33fc0aecbf0249e3b5a6a40a7e
daniel <dpletzke@gmail.com>
2021-02-11 01:05:41 -0600

    send axios request for exams of many sections

--- client/src/components/AccountPage.jsx
+++ client/src/components/AccountPage.jsx
@@ -1,59 +1,57 @@
-import { useContext, useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import Typography from "@material-ui/core/Typography";
 
 import useAxios from "../hooks/useAxios";
 import Nav from "./Nav";
+import { Box } from "@material-ui/core";
 
 const baseURL = "http://localhost:3001";
 
 const AccountPage = (props) => {
   const { setToken } = props;
-  console.log(userId);
-  let classCodes = [
-    "STAT 100",
-    "HIST 220",
-    "PHIL 230",
-    "CALC 232",
-    "ENG 330",
-    "PSY 395",
-    "ANTH 440",
-  ];
   const pageTitle = "🧬 DNAducation";
-  let user = { name: "Devin Sanders", email: "dsand@gmail.com" };
 
-  const axios = useAxios();
+  const userId = localStorage.getItem("userId");
+
+  const [classCodes, setClassCodes] = useState([]);
+  const [exams, setExams] = useState([]);
+
+  const axios = useAxios(sessionStorage.getItem("jwt"));
 
   useEffect(() => {
-    // get class codes for user
     if (userId) {
-      let sectionsURL = baseURL + `/users/${userId}/sections`;
-      console.log(sectionsURL);
-      const sectionsReq = axios.get(sectionsURL);
-      // const exams = axios.get(baseURL + `/sections/exams`, [sectionIds]);
-      // get class titles, descriptions, section teacher, section ids
+      const sectionsURL = baseURL + `/users/${userId}/sections`;
+      const examsUrl = baseURL + `/sections/exams`;
 
-      console.log(sectionsReq);
-
-      sectionsReq.then((result) => {
-        console.log("what?", result);
-      });
+      axios
+        .get(sectionsURL)
+        .then(({ data: sections }) => {
+          const classCodes = sections.map((sec) => sec.code);
+          const sectionIds = sections.map((sec) => sec.section_id);
+          setClassCodes(classCodes);
+          return axios.get(examsUrl, { params: { sectionIds } });
+        })
+        .then(({ data: exams }) => {
+          setExams(exams);
+        })
+        .catch((err) => console.error(err));
     }
-  }, [userId]);
+  }, []);
 
   const navProps = {
     setToken,
     classCodes,
     pageTitle,
-    user,
   };
+
   return (
     <Nav {...navProps}>
-      <Typography>
-        Et suscipit doloribus eos qui magni ad. Odio deserunt enim odio tempora
-        commodi. Sunt corrupti voluptas quas eos quia. Maiores deserunt eius
-        atque rerum quibusdam. Totam unde est quo neque cum.
-      </Typography>
+      <Box>Name: Email: </Box>
+      <Box>Click Here to record your TypeDNA!</Box>
+      {exams.map((exam) => (
+        <Typography>{JSON.stringify(exam)}</Typography>
+      ))}
     </Nav>
   );
 };
