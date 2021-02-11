1b55607dc148297adb110a9ebe402aa3732c820b
daniel <dpletzke@gmail.com>
2021-02-10 22:14:35 -0600

    remove userid conetxt and work on axios for sections

--- client/src/components/AccountPage.jsx
+++ client/src/components/AccountPage.jsx
@@ -3,13 +3,13 @@ import { useContext, useEffect } from "react";
 import Typography from "@material-ui/core/Typography";
 
 import useAxios from "../hooks/useAxios";
-import UserIdContext from "../contexts/UserIdContext";
 import Nav from "./Nav";
+
+const baseURL = "http://localhost:3001";
 
 const AccountPage = (props) => {
   const { setToken } = props;
-  const userId = useContext(UserIdContext);
-
+  console.log(userId);
   let classCodes = [
     "STAT 100",
     "HIST 220",
@@ -23,6 +23,23 @@ const AccountPage = (props) => {
   let user = { name: "Devin Sanders", email: "dsand@gmail.com" };
 
   const axios = useAxios();
+
+  useEffect(() => {
+    // get class codes for user
+    if (userId) {
+      let sectionsURL = baseURL + `/users/${userId}/sections`;
+      console.log(sectionsURL);
+      const sectionsReq = axios.get(sectionsURL);
+      // const exams = axios.get(baseURL + `/sections/exams`, [sectionIds]);
+      // get class titles, descriptions, section teacher, section ids
+
+      console.log(sectionsReq);
+
+      sectionsReq.then((result) => {
+        console.log("what?", result);
+      });
+    }
+  }, [userId]);
 
   const navProps = {
     setToken,
