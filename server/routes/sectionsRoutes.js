6e5d08303411e661bc75a6598de212c819526461
daniel <dpletzke@gmail.com>
2021-02-11 18:12:45 -0600

    remove uneeded code

--- server/routes/sectionsRoutes.js
+++ server/routes/sectionsRoutes.js
@@ -19,7 +19,7 @@ module.exports = ({
       .catch((err) => next(err));
   });
 
-  // create sections
+  // create section
   router.post("/", (req, res, next) => {
     createSection(req.body)
       .then((sections) => {
@@ -28,45 +28,12 @@ module.exports = ({
       .catch((err) => next(err));
   });
 
-  // get section by id
-  // router.get("/:sectionId", (req, res, next) => {
-  //   const {sectionId} = req.params;
-  //   getSectionById(sectionId)
-  //     .then((section) => {
-  //       res.json(section);
-  //     })
-  //     .catch((err) => next(err));
-  // });
-
   // get all exams for provided sections
   router.get(`/exams`, (req, res, next) => {
     const { sectionIds } = req.query;
     getExamsBySections(sectionIds)
       .then((result) => {
         if (!result.length) throw new ErrorHandler(404, "Not found");
-        res.json(result);
-      })
-      .catch((err) => next(err));
-  });
-
-  // insert all questions for an exam
-  router.post(`/:examId/questions`, (req, res, next) => {
-    const { examId } = req.params;
-    const { questions } = req.body;
-
-    setQuestionsByExam(examId, questions)
-      .then((result) => {
-        res.json(result);
-      })
-      .catch((err) => next(err));
-  });
-
-  // delete all questions for an exam
-  router.delete(`/:examId/questions`, (req, res, next) => {
-    const { examId } = req.params;
-    
-    deleteQuestionsByExam(examId)
-      .then((result) => {
         res.json(result);
       })
       .catch((err) => next(err));
