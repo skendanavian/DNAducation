283b1b603741d201769cc2bdfb57abdb9a978a16
skendanavian <s.nissen.bass@gmail.com>
2021-02-14 23:37:25 -0600

    refactor

--- client/src/components/TypeDnaModal.jsx
+++ client/src/components/TypeDnaModal.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { useState } from "react";
+
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogContentText from "@material-ui/core/DialogContentText";
-import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
 import TypeDNA from "../typeDna/typingdna";
 import highlightWords from "highlight-words";
 import generateAxios from "../helpers/generateAxios";
 import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
   root: {
