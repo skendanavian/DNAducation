e23f25bffc215c4b863a2f605a9e9d91c5ea3756
daniel <dpletzke@gmail.com>
2021-02-15 02:25:43 -0600

    adjust for new contentView state object

--- client/src/components/SubjectIcon.jsx
+++ client/src/components/SubjectIcon.jsx
@@ -6,20 +6,34 @@ import HistoryIcon from "@material-ui/icons/History";
 import PeopleAltIcon from "@material-ui/icons/PeopleAlt";
 import NaturePeopleIcon from "@material-ui/icons/NaturePeople";
 import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import SchoolIcon from "@material-ui/icons/School";
+
+import Badge from "@material-ui/core/Badge";
 
 export default function SubjectIcon(props) {
-  const { text } = props;
+  const { text, type } = props;
   if (text === "Account") return <AccountCircleIcon />;
   const subject = text && text.split(" ")[0].toUpperCase();
-  return (
-    {
-      STAT: <EqualizerIcon />,
-      PHIL: <AirlineSeatReclineExtraIcon />,
-      CALC: <FunctionsIcon />,
-      ENG: <BookIcon />,
-      HIST: <HistoryIcon />,
-      PSY: <PeopleAltIcon />,
-      ANTH: <NaturePeopleIcon />,
-    }[subject] || <BookIcon />
-  );
+  const subjectIcon = {
+    STAT: <EqualizerIcon />,
+    PHIL: <AirlineSeatReclineExtraIcon />,
+    CALC: <FunctionsIcon />,
+    ENG: <BookIcon />,
+    HIST: <HistoryIcon />,
+    PSY: <PeopleAltIcon />,
+    ANTH: <NaturePeopleIcon />,
+  }[subject] || <BookIcon />;
+
+  if (type && type === "Teacher") {
+    return (
+      <Badge
+        anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+        badgeContent={<SchoolIcon color="primary" />}
+      >
+        {subjectIcon}
+      </Badge>
+    );
+  }
+
+  return subjectIcon;
 }
