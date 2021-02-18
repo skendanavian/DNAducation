032481e4e7c34e7a425ec1cb3e4be097af0f2539
daniel <dpletzke@gmail.com>
2021-02-18 12:57:17 -0600

    try to import dotenv in app.js on top of file asd default

--- server/app.js
+++ server/app.js
@@ -6,6 +6,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 
+require('dotenv').config();
+
 // Environment setup
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
