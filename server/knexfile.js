9ecfd4432879d587cf888877fa2d7bf2391b26c4
daniel <dpletzke@gmail.com>
2021-02-18 13:30:24 -0600

    try hail mary ssl

--- server/knexfile.js
+++ server/knexfile.js
@@ -20,13 +20,11 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    // connection: {
-    //   host:process.env.DATABASE_URL,
-    //   user: process.env.DB_USER,
-    //   password: process.env.DB_PASS,
-    //   database: process.env.DB_NAME,
-    // },
+    // connection: process.env.DATABASE_URL,
+    connection: {
+      host:process.env.DATABASE_URL,
+      ssl: { rejectUnauthorized: false }
+    },
     migrations: {
         directory: __dirname + '/db/migrations',
       },
