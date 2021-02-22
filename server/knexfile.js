fdf0811ec42ae48698271aa144096546b9c28f94
daniel <dpletzke@gmail.com>
2021-02-22 00:59:47 -0600

    create regex split of conn string provided by heroku to db params, required because pg sets ssl on by default, which were not using and turning off ssl makes a split creds required

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,9 @@
 require("dotenv").config({ path: "./.env" });
 
+const conn = process.env.DATABASE_URL;
+const [__, user, password, host, port, database] = conn.split(/[/:@]+/g);
+
+
 module.exports = {
   development: {
     client: "pg",
@@ -22,14 +26,12 @@ module.exports = {
     client: 'pg',
     // connection: process.env.DATABASE_URL,
     connection: {
-      host:"ec2-54-198-73-79.compute-1.amazonaws.com",
-      database: "d35vh2lj2daeq5",
-      user: "tofshwckvdsnug",
-      port: 5432,
-      password: "84c4e1a6a609681393b66684cc6eb580a07e9399e758cf56661d03d8f508d34d",
+      user,
+      password,
+      host,
+      port,
+      database,
       ssl: { rejectUnauthorized: false }
-
-
     },
     migrations: {
         directory: __dirname + '/db/migrations',
@@ -38,21 +40,4 @@ module.exports = {
         directory: __dirname + '/db/seeds',
       },
   },
-  // production: {
-  //   client: "pg",
-  //   connection: {
-  //     host: 'dnaducation-instance-1.c5rq2kwuwipy.us-east-2.rds.amazonaws.com',
-  //     port: 5432,
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASS,
-  //     database: process.env.DB_NAME,
-  //   },
-  //   // debug:true,
-  //   migrations: {
-  //     directory: "./db/migrations",
-  //   },
-  //   seeds: {
-  //     directory: "./db/seeds",
-  //   },
-  // },
 };
